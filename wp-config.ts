fd77accf111e471d2a36c86f149f2527d1d855b6
unknown <ngocchiem2408@gmail.com>
2022-08-08 23:20:01 +0700

    update

--- wp-config.ts
+++ wp-config.ts
@@ -1,3 +1,8 @@
+abc: {
+   test1: '${JAVAAPP_DEV_DB_USER}',
+   test2: '${JAVAAPP_DEV_DB_PASS}'
+},
+
 export const dbuser: string  = '${JAVAAPP_DEV_DB_USER}'
 export const dppass: string = '${JAVAAPP_DEV_DB_PASS}'
 
